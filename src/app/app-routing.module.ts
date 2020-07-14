eebe9a6a64fb40a5e287de9b987911a7d2cd83a8
unknown <ghernandezhack@gmail.com>
2020-07-14 00:17:53 -0600

    Creacion de Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }, { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full',
+  },
+
+  { path: 'character-list', loadChildren: () => 
+    import(
+      './components/pages/characters/character-list/character-list.module'
+      ).then(m => m.CharacterListModule) }, 
+
+  { path: 'home', loadChildren: () => import(
+      './components/pages/home/home.module'
+      ).then(m => m.HomeModule) }, 
+      
+  { path: 'character-details', loadChildren: () => import(
+    './components/pages/characters/character-details/character-details.module'
+    ).then(m => m.CharacterDetailsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
