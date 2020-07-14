4fa3cbb19af8d73735bd92e0045de427a4b2f463
unknown <ghernandezhack@gmail.com>
2020-07-13 20:11:54 -0600

    Añadiendo modulos y componentes al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }, { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
