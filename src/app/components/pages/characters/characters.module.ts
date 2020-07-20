1deddd8d87034d425919a4dbf114811730b8e5f2
unknown <ghernandezhack@gmail.com>
2020-07-19 23:14:49 -0600

    Agregando components en Characters

--- src/app/components/pages/characters/characters.module.ts
+++ src/app/components/pages/characters/characters.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
+import { CharacterListComponent } from '@characters/character-list/character-list.component';
 
-
+const myComponents = [CharacterDetailsComponent, CharacterListComponent];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...myComponents
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    ...myComponents
+  ],
 })
 export class CharactersModule { }
--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -11,11 +11,13 @@ export class CharacterService {
   constructor(private http:HttpClient) { }
 
   searchCharacters(query = '', page = 1){
-    return this.http.get<Character[]>(`${environment.baseUrlAPI}/?name=${query}&page=${page}`)
+
+    const filter = `${environment.baseUrlAPI}/?name=${query}&page=${page}`
+    return this.http.get<Character[]>(filter);
   }
 
   getDetails(id:number){
+    return this.http.get<Character>(`${environment.baseUrlAPI}/${id}`);
   }
-  return this.http.get<Character>(`${environment.baseUrlAPI}/${id}`)
 
 }
