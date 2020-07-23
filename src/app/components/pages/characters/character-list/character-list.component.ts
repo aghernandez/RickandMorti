15a93f9ca7f2b5d70a08d077be323e54f9940f53
unknown <ghernandezhack@gmail.com>
2020-07-23 17:27:49 -0600

    Añadiendo funcionalidad

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '@app/shared/services/character.service';
+import { take } from "rxjs/operators";
+import { Character } from '@app/shared/interfaces/character.interface';
+
+type RequestInfo = {
+  next: string;
+};
 
 @Component({
   selector: 'app-character-list',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-list.component.css']
 })
 export class CharacterListComponent implements OnInit {
+  characters: Character[] = [];
+  info: RequestInfo = {
+    next: null,
+  };
 
-  constructor() { }
+  private pageNum=1;
+  private query: string;
+  private hideScrollHeight = 200;
+  private showScrollHeright = 500;
+
+  constructor(private characterSvc: CharacterService) { }
 
   ngOnInit(): void {
+  }
+
+  private getDataFromService ():void{
+    this.characterSvc.searchCharacters(this.query, this.pageNum)
+    .pipe(
+      take(1)
+    ).subscribe((res:any) =>{
+      console.log('Response ->', res);
+      const {info, results } = res;
+      this.characters = [... this.characters, ...results];
+      this.info = info;
+    })
   }
 
 }
